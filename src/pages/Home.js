bae3f55950b27e7da90264e82b0f7dc1f6858ce9
EmmanuelOluwafemi <seek9ja@gmail.com>
2021-10-27 09:10:57 +0100

    make hero component reuseable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,16 @@ const Home = () => {
                 `}
             />
             <Work />
-            <Hero />
+            <Hero 
+                title="Lorem ipsum dolor sit amet, consectetur adipiscing." 
+                subTitle="Etiam fusce elit ac nunc orci ac enim."
+                content={`
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Et convallis lacus, eget suspendisse. A varius mattis 
+                    laoreet enim viverra ligula eget. Volutpat et, 
+                    nec dui luctus aliquam tortor.
+                `}
+            />
             <MoreInfo />
             <Projects />
             <About />
