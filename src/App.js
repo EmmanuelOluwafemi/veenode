c4db27a70028cd603d7dca127340c4fc444818d9
Tuasegun <Tuase360@gmail.com>
2021-10-28 22:26:51 +0100

    blogPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Home from "./pages/Home";
-import { BrowserRouter } from "react-router-dom";
+import Blogs from "./pages/Blogs"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 function App() {
   return (
+    <BrowserRouter>
     <>
-      <BrowserRouter>
-      <Home />
-      </BrowserRouter>
+      <Switch>
+      <Home exact path="/" />
+      <Route exact path="/blogs">
+      <Blogs />
+      </Route>
+      </Switch>
     </>
+    </BrowserRouter>
   );
 }
 
